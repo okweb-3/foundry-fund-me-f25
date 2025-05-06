fceb5ed1fe0b8412db80a49e3db3afe15a6d537b
okweb-3 <fortwu06@gmail.com>
2025-05-07 00:34:06 +0800

    Finish MakeFile

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {PriceConverter} from "./PriceConverter.sol";
-import {AggregatorV3Interface} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 error FundMe__NotOwner();
 
